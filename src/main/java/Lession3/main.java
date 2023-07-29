bb8a1af22c0d7f0c63fb8ffd016a73bb69a5d09b
TeresaTao127 <taoleiluck2020@gmail.com>
2023-07-29 20:33:01 +0100

    lesson3 Movie

--- src/main/java/Lession3/main.java
+++ src/main/java/Lession3/main.java
@@ -1,5 +1,7 @@
 package Lession3;
 
+import java.sql.SQLOutput;
+
 public class main {
     public static void main(String[] args) {
         Seat seat1 = new Seat(2, 5);
@@ -16,5 +18,14 @@ public class main {
             System.out.println("The seat is available");
         }
 
+        Movie movie1 = new Movie("The Lord of the Rings", "Peter Jackson", 201, 2003);
+        System.out.println("Title: " + movie1.getTitle());
+        System.out.println("Director: " + movie1.getDirector());
+        System.out.println("Duration of minutes: " + movie1.getDurationMinutes());
+        System.out.println("Year of production: " + movie1.getYearOfProduction());
+
+        System.out.println(movie1);
     }
+
+
 }
