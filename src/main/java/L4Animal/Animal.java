477c5875b85a698581a9d60d07ff522af1d28392
TeresaTao127 <taoleiluck2020@gmail.com>
2023-07-31 16:06:01 +0100

    Task4Animal

---
+++ src/main/java/L4Animal/Animal.java
@@ -0,0 +1,11 @@
+package L4Animal;
+
+public abstract class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/L4Animal/Subclass.java
@@ -0,0 +1,7 @@
+package L4Animal;
+
+public class Subclass extends Animal{
+    public Subclass(String name) {
+        super(name);
+    }
+}
